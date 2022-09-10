a3fa8cb2295824ca73899cd087faa35e8736b6b5
frankson souza <frankson31@gmail.com>
2022-09-10 16:25:38 -0300

    colocando flatlist

--- components/Feed.js
+++ components/Feed.js
@@ -1,80 +1,97 @@
-import { StyleSheet, Text, View, Image, } from 'react-native';
+import { StyleSheet, Text, View, Image, FlatList } from 'react-native';
 import { FontAwesome5 } from '@expo/vector-icons';
 
 export default function Feed() {
+    const posts = [
+        {
+            id: 1,
+            nome: 'Pernalonga',
+            imgPerfil: require('../assets/images/pernalonga.jpg'),
+            img: require('../assets/images/pernalonga.jpg'),
+            aspectRatio: 0.756,
+        },
+        {
+            id: 2,
+            nome: 'Pernalonga',
+            imgPerfil: require('../assets/images/pernalonga.jpg'),
+            img: require('../assets/images/pernalonga.jpg'),
+            aspectRatio: 0.756,
+        },
+        {
+            id: 3,
+            nome: 'Pernalonga',
+            imgPerfil: require('../assets/images/pernalonga.jpg'),
+            img: require('../assets/images/pernalonga.jpg'),
+            aspectRatio:  0.756,
+        }
+    ];
+
+    function RenderItem({ item }) {
+        return (
+            <View style={styles.post}>
+                <View style={styles.postheader}>
+                    <View style={styles.postheaderesquerda}>
+                        <Image style={styles.postheaderimg} source={item.imgPerfil} />
+                        <Text>{item.nome}</Text>
+                    </View>
+                    <FontAwesome5 name='ellipsis-h' size={16} color='black' />
+                </View>
+                    <Image style={styles.postimg} aspectRatio={item.aspectRatio} source={item.img} />
+                <View style={styles.footer}>
+                    <FontAwesome5 style={styles.footericon} name='heart' size={36} color='black' />
+                    <FontAwesome5 style={styles.footericon} name='comment' size={36} color='black' />
+                    <View />
+                </View>
+            </View>
+        );
+    }
     return (
-        <View style={styles.feed}>
-            <View style={styles.post}>
-                <View style={styles.postheader}>
-                    <View style={styles.postheaderesquerda}>
-                        <Image style={styles.postheaderimg} source={require('../assets/images/pernalonga.jpg')} />
-                        <Text>pernalonga</Text>
-                    </View>
-                    <FontAwesome5 name='ellipsis-h' size={16} color='black' />
-                </View>
-                <Image style={styles.postimg} aspectRatio={1.777} source={require('../assets/images/pernalonga.jpg')} />
-            </View>
-            <View style={styles.footer}>
-                <FontAwesome5 style={styles.footericon} name='heart' size={36} color='black' />
-                <FontAwesome5 style={styles.footericon} name='comment' size={36} color='black' />
-                <View />
-            </View>
-            <View style={styles.post}>
-                <View style={styles.postheader}>
-                    <View style={styles.postheaderesquerda}>
-                        <Image style={styles.postheaderimg} source={require('../assets/images/pernalonga.jpg')} />
-                        <Text>pernalonga</Text>
-                    </View>
-                    <FontAwesome5 name='ellipsis-h' size={16} color='black' />
-                </View>
-                <Image style={styles.postimg} aspectRatio={1.777} source={require('../assets/images/pernalonga.jpg')} />
-            </View>
-            <View style={styles.footer}>
-                <FontAwesome5 style={styles.footericon} name='heart' size={36} color='black' />
-                <FontAwesome5 style={styles.footericon} name='comment' size={36} color='black' />
-                <View />
-            </View>
-        </View>
+        <FlatList
+            data={posts}
+            renderItem={RenderItem}
+            keyExtractor={item=>item.id}
+            showsVerticalScrollIndicator={false}
+        />
     );
 }
 
 const styles = StyleSheet.create({
     feed: {
-      flex: 1,
-      backgroundColor: '#fff',
+        flex: 1,
+        backgroundColor: '#fff',
     },
     post: {
-      backgroundColor: '#fff',
+        backgroundColor: '#fff',
     },
     postheader: {
-      flexDirection: 'row',
-      height: 50,
-      alignItems: 'center',
-      justifyContent: 'space-between',
-      margin: 5,
+        flexDirection: 'row',
+        height: 50,
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        margin: 5,
     },
     postheaderesquerda: {
-      flexDirection: 'row',
-      alignItems: 'center',
+        flexDirection: 'row',
+        alignItems: 'center',
     },
     postheaderimg: {
-      height: 50,
-      width: 50,
-      borderRadius: 25,
-      marginRight: 5,
+        height: 50,
+        width: 50,
+        borderRadius: 25,
+        marginRight: 5,
     },
     postimg: {
-      width: '100%',
-      height: undefined,
+        width: '100%',
+        height: undefined,
     },
     footer: {
-      height: 60,
-      backgroundColor: '#fff',
-      flexDirection: 'row',
-      alignItems:'center',
-      padding: 5,
+        height: 60,
+        backgroundColor: '#fff',
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 5,
     },
-    footericon:{
-      margin:5,
+    footericon: {
+        margin: 5,
     }
-  });
+});
--- components/Stories.js
+++ components/Stories.js
@@ -1,51 +1,75 @@
-import { StyleSheet, Text, View, Image, } from 'react-native';
+import { StyleSheet, Text, View, Image, FlatList } from 'react-native';
 
 export default function Stories() {
+    const stories = [
+        {
+            id: 1,
+            nome: 'Perna',
+            src: require('../assets/images/pernalonga.jpg')
+        },
+        {
+            id: 2,
+            nome: 'Perna',
+            src: require('../assets/images/pernalonga.jpg')
+        },
+        {
+            id: 3,
+            nome: 'Perna',
+            src: require('../assets/images/pernalonga.jpg')
+        },
+        {
+            id: 4,
+            nome: 'Perna',
+            src: require('../assets/images/pernalonga.jpg')
+        },
+        {
+            id: 5,
+            nome: 'Perna',
+            src: require('../assets/images/pernalonga.jpg')
+        },
+        {
+            id: 6,
+            nome: 'Perna',
+            src: require('../assets/images/pernalonga.jpg')
+        }
+    ];
+
+    function RenderItem({ item }) {
+        return (
+            <View style={styles.story}>
+                <Image style={styles.imstory} source={item.src} />
+                <Text>{item.nome}</Text>
+            </View>
+        );
+    }
     return (
         <View style={styles.stories}>
-            <View style={styles.story}>
-                <Image style={styles.imstory} source={require('../assets/images/pernalonga.jpg')} />
-                <Text>Perna</Text>
-            </View>
-            <View style={styles.story}>
-                <Image style={styles.imstory} source={require('../assets/images/pernalonga.jpg')} />
-                <Text>Perna</Text>
-            </View>
-            <View style={styles.story}>
-                <Image style={styles.imstory} source={require('../assets/images/pernalonga.jpg')} />
-                <Text>Perna</Text>
-            </View>
-            <View style={styles.story}>
-                <Image style={styles.imstory} source={require('../assets/images/pernalonga.jpg')} />
-                <Text>Perna</Text>
-            </View>
-            <View style={styles.story}>
-                <Image style={styles.imstory} source={require('../assets/images/pernalonga.jpg')} />
-                <Text>Perna</Text>
-            </View>
-            <View style={styles.story}>
-                <Image style={styles.imstory} source={require('../assets/images/pernalonga.jpg')} />
-                <Text>Perna</Text>
-            </View>
+            <FlatList
+                data={stories}
+                renderItem={RenderItem}
+                keyExtractor={item =>item.id}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+            />
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     stories: {
-      flexDirection: 'row',
-      height: 90,
-      backgroundColor: '#fff'
+        flexDirection: 'row',
+        height: 90,
+        backgroundColor: '#fff'
     },
     story: {
-      height: 90,
-      width: 90,
-      backgroundColor: '#fff',
-      alignItems: 'center'
+        height: 90,
+        width: 90,
+        backgroundColor: '#fff',
+        alignItems: 'center'
     },
     imstory: {
-      width: 70,
-      height: 70,
-      borderRadius: 35,
+        width: 70,
+        height: 70,
+        borderRadius: 35,
     },
-  });
+});
